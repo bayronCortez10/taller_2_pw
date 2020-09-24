fd2f74a3014e909d9d14b45b580e977533e07bd9
ManoloPin <jpinillaca1@uniminuto.edu.co>
2020-09-24 17:59:47 -0500

    cambios

---
+++ cliente/fetchh.js
@@ -0,0 +1,36 @@
+//Listeners
+document.getElementById("term").addEventListener("keyup", exe);
+
+
+function exe(event) {
+
+   if (event.keyCode == 13) {
+      const data = new FormData();
+      data.append('content', document.getElementById("term").value);
+
+      fetch(
+         '../servidor/clases/mensaje.php',
+
+         {
+            method: 'POST',
+            body: data
+         }
+      )
+         .then(function (response) {
+            if (response.ok) {
+               return response.text()
+               //console.log(response.json())
+            } else {
+               throw "Error en la llamada Fetch";
+            }
+
+         })
+         .then(function (respuestaDelServidor) {
+            document.getElementById('term').value = respuestaDelServidor;
+            console.log(respuestaDelServidor);
+         })
+         .catch(function (err) {
+            console.log(err);
+         });
+   }
+}
--- servidor/clases/mensaje.php
+++ servidor/clases/mensaje.php
@@ -1,1 +1,13 @@
 <?php
+
+if (isset($_POST['content'])){
+   echo "hasta aca conecta";
+  
+//   SESSION_START();
+//    print_r($_SESSION['term']);
+//}
+//else if (!isset($_POST['registrarMensaje'])){
+//    
+//    $_SESSION['term']=array();
+}
+
